1db0ce0e97b75701fd197728ca5169392ed04853
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2015-12-12 17:43:25 +0100

    Cinematicas del primer nivel

--- Assets/Cinematics/Scripts/ControlCinematic.cs
+++ Assets/Cinematics/Scripts/ControlCinematic.cs
@@ -3,8 +3,44 @@ using System.Collections;
 
 public class ControlCinematic : MonoBehaviour {
 
+	public GameObject canvasCinematico;
+
 	public void showHeroes(){
 
 
 	}
+
+	public void enableCanvasCinematico(){
+
+		canvasCinematico.SetActive(true);
+		
+	}
+
+	public void disableCanvasCinematico(){
+		
+		canvasCinematico.SetActive(false);
+		
+	}
+
+	public void cambiarRetrato(GameObject retrato){
+		
+
+		
+	}
+
+	
+	public void cambiarNombre(string nombre){
+		
+
+		
+	}
+
+	
+	public void cambiarTexto(string texto){
+		
+
+		
+	}
+
+
 }
