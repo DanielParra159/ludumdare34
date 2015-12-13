c63e60c6fca12c6e5ffc943ccbd81495d4201236
Daniel <dani.parra.90@gmail.com>
2015-12-13 17:08:35 +0100

    Tooltip preparados

--- Assets/Scripts/Gui/GUITooltip.cs
+++ Assets/Scripts/Gui/GUITooltip.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(EventTrigger))]
 public class GUITooltip : MonoBehaviour {
+
+    
+    FeedbackMessagesManager m_feedbackMessagesManager;
+    [Tooltip("Posicion donde se va a mostrar")]
+    public FeedbackMessagesManager.POSITIONS position;
+    [Tooltip("texto")]
+    public string text;
 
 	// Use this for initialization
 	void Start () {
-	
+        m_feedbackMessagesManager = FeedbackMessagesManager.instance;
 	}
 	
 	// Update is called once per frame
@@ -15,12 +24,12 @@ public class GUITooltip : MonoBehaviour {
 
     public void OnMouseEnter()
     {
-
+        m_feedbackMessagesManager.showCameraMessage(text, position, true);
         Debug.Log("Enter");
     }
     public void OnMouseExit()
     {
-
+        m_feedbackMessagesManager.hideCameraMessage();
         Debug.Log("Exit");
     }
 
